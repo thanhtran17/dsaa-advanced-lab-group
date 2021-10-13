4c2b4d1a664d9581a98280f7a6323c3e82499c74
Trần Phúc Thành <enigma15012001@gmail.com>
2021-10-13 09:13:22 +0700

    Update TranPhucThanh - Week 2.c

--- TranPhucThanh - Week 2.c
+++ TranPhucThanh - Week 2.c
@@ -2,6 +2,7 @@
 #include<stdbool.h>
 #include<stdlib.h>
 #include<time.h>
+#define num 100
 //----------------------------------------------------------------------------
 int compare(void *x, void *y)
 {
@@ -18,7 +19,7 @@ int search(
            int size, 
            int l, int r, 
            void *item, 
-           int (*int_compare) (void*, void*)
+           int (*compare) (void*, void*)
           )
 {
   int i, res;
@@ -28,9 +29,9 @@ int search(
     if (res == 0)
       return i;
     else if (res < 0)
-      return search(buf, size, l, i - 1, item, int_compare);
+      return search(buf, size, l, i - 1, item, compare);
     else 
-      return search(buf, size, i + 1, r, item, int_compare);
+      return search(buf, size, i + 1, r, item, compare);
   }
 }
 //----------------------------------------------------------------------------
@@ -46,18 +47,15 @@ void printArray(int a[], int n)
 int main()
 {
   int i, res, item = 11;
-  // int n = 7;
-  // int a[7] = {10, 80, 30, 90, 40, 50, 70};
 
-  int n = 100;
-  int a[100];
+  int a[num];
   srand(time(NULL));
-  for (i = 0; i < n; i++)
-    a[i] = rand() % n;
+  for (i = 0; i < num; i++)
+    a[i] = rand() % num;
   
-  qsort(a, n, sizeof(int), compare);
-  printArray(a, n);
-  res = search(a, sizeof(int), 0, n - 1, &item, compare);
+  qsort(a, num, sizeof(int), compare);
+  printArray(a, num);
+  res = search(a, sizeof(int), 0, num - 1, &item, compare);
   printf("Index for item: \n%d\n", res);
   return 0;
 }
