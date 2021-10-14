c1a12f797a727423037b21e640d13b23a5b5ffc2
Trần Phúc Thành <enigma15012001@gmail.com>
2021-10-14 22:12:37 +0700

    Update quiz1.c

--- Thanh/quiz1.c
+++ Thanh/quiz1.c
@@ -7,12 +7,12 @@
 #define INITIAL_SIZE 10
 #define INCREMENTAL_SIZE 5
 //----------------------------------------------------------------------
-typedef struct {
+typedef struct PhoneEntry{
   char name[80];
   char number[11];
-} PhoneEntry;
+} PhoneEntry[INITIAL_SIZE];
 //
-typedef struct {
+typedef struct PhoneBook {
   PhoneEntry* entries;
   int total;
   int size;
@@ -21,28 +21,54 @@ typedef struct {
 PhoneBook* createPhoneBook();
 void dropPhoneBook(PhoneBook* book);
 void addPhoneNumber(char name[], char number[], PhoneBook* book);
-PhoneEntry *getPhoneNumber(char* name, PhoneBook book);
+void getPhoneNumber(char* name, PhoneBook* book);
 void printPhoneBook(PhoneBook* book);
 //----------------------------------------------------------------------
 int main()
 {
   char entryName[80], entryNumber[80];
+  int choice;
 
   printf("--> New book created! \n");
   PhoneBook* book = createPhoneBook();
   printf("%d %d %d\n", book->entries, book->total, book->size);
 
   //
-  printf("Entering new phone entry...\n");
-  printf("  - Enter entry's name: ");
-  scanf("%s", entryName);
-  printf("  - Enter entry's number: ");
-  scanf("%s", entryNumber);
-  addPhoneNumber(entryName, entryNumber, book);
+  while (true){
+    char addContinue[4];
+
+    printf("ENTERING NEW PHONE ENTRY...\n");
+    printf("  - Enter entry's name: ");
+    scanf("%s", entryName);
+    printf("  - Enter entry's number: ");
+    scanf("%s", entryNumber);
+    addPhoneNumber(entryName, entryNumber, book);
+
+    printf("?? Continue adding? (Yes/No): ");
+    scanf("%s", addContinue);
+    if (strcmp(addContinue, "No") == 0)
+      break;
+  }
+  
+  //
+  while (true)
+  {
+    char enteredName[80];
+    char findContinue[4];
+
+    printf("\nENTER NAME TO GET PHONE NUMBER: ");
+    scanf("%s", enteredName);
+    getPhoneNumber(enteredName, book);
+
+    printf("?? Continue finding? (Yes/No): ");
+    scanf("%s", findContinue);
+    if (strcmp(findContinue, "No") == 0)
+      break;
+  }
 
   //
   dropPhoneBook(book);
-  printf("\n-->Book deleted!\n");
+  printf("\n--> Book deleted!\n");
 
   return 0;
 }
@@ -63,22 +89,34 @@ void dropPhoneBook(PhoneBook* book)
 //----------------------------------------------------------------------
 void addPhoneNumber(char* name, char* number, PhoneBook* book)
 {
+  (book->total)++;
+  printf("Total book(s): %d\n", book->total);
+  
   // if the entry exists, the value should be overwritten
+  strcpy(book->entries[book->total]->name, name);
+  strcpy(book->entries[book->total]->number, number);
 
-  //  
-
-  strcpy(book->entries->name, name);
-  strcpy(book->entries->number, number);
-
-  printf("%s %s\n", book->entries->name, book->entries->number);
+  printf("Entry added: \n");
 
   if (book->size == book->total){
     book->entries = (PhoneEntry*) realloc(book->entries, sizeof(PhoneEntry) * (book->total + INCREMENTAL_SIZE));
     book->size += INCREMENTAL_SIZE;
   }
+
+  printf("Print all entries' names: \n");
+  for (int i = 1; i <= book->total; i++)
+    printf("--> %s %s\n", book->entries[i]->name, book->entries[i]->number);
+  printf("\n");
 }
 //----------------------------------------------------------------------
-PhoneEntry *getPhoneNumber(char* name, PhoneBook book){
-
-}
+void getPhoneNumber(char* name, PhoneBook* book){
+  for (int i = 1; i <= book->total; i++)
+    if (strcmp(name, book->entries[i]->name) == 0){
+      printf("--> Find: %s\n\n",(book->entries[i])->number);
+      return ;
+    }
+  
+  printf("--> Can't find.\n\n");
+  return;
+} 
 //----------------------------------------------------------------------
