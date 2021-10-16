2662277ad4d8752a2a2d11c171be9c127d026987
Your Name <longdieu12x@gmail.com>
2021-10-16 22:25:15 +0700

    Minh Dang: Add week 3 assignment

---
+++ Commits week 3/DDT - Week 3 - Quiz 2.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define INITIAL_SIZE 1
+#define INCREMENTAL_SIZE 10
+
+typedef struct
+{
+    void *key;
+    void *value;
+} Entry;
+typedef struct
+{
+    Entry *entries;
+    int size, total;
+    Entry (*makeNode)(void *, void *);
+    int (*compare)(void *, void *);
+} SymbolTable;
+Entry makePhone(void *name, void *phone)
+{
+    Entry res;
+    res.key = strdup((char *)name);
+    res.value = malloc(sizeof(long));
+    memcpy(res.value, phone, sizeof(long));
+    return res;
+}
+int comparePhone(void *key1, void *key2)
+{
+    return strcmp((char *)key1, (char *)key2);
+}
+SymbolTable *createSymbolTable(Entry makeNode(void *, void *), int compare(void *, void *))
+{
+    SymbolTable *newTable = (SymbolTable *)malloc(sizeof(SymbolTable));
+    newTable->size = INITIAL_SIZE;
+    newTable->total = 0;
+    newTable->makeNode = makeNode;
+    newTable->compare = compare;
+    newTable->entries = (Entry *)malloc(sizeof(Entry) * newTable->size);
+    return newTable;
+}
+int binarySearch(Entry *entries, int l, int r, void *key)
+{
+    if (r >= l)
+    {
+        int mid = l + (r - l) / 2;
+        printf("%s %s\n", (char *)entries[mid].key, (char *)key);
+        if (comparePhone(entries[mid].key, key) == 0)
+            return mid;
+        if (comparePhone(entries[mid].key, key) > 0)
+            return binarySearch(entries, l, mid - 1, key);
+        return binarySearch(entries, mid + 1, r, key);
+    }
+    return -1;
+}
+
+void addEntry(void *key, void *value, SymbolTable *book)
+{
+    int pivot = binarySearch(book->entries, 0, book->total - 1, key);
+    if (pivot == -1)
+    {
+        if (book->total == book->size)
+        {
+            book->size += INCREMENTAL_SIZE;
+            book->entries = (Entry *)realloc(book->entries, sizeof(Entry) * book->size);
+        }
+        book->entries[book->total++] = makePhone(key, value);
+    }
+    else
+    {
+        memcpy(book->entries[pivot].value, value, sizeof(long));
+    }
+}
+void dropSymbolTable(SymbolTable *book)
+{
+    free(book->entries);
+    free(book);
+}
+Entry *getEntry(void *key, SymbolTable *book)
+{
+    for (int i = 0; i < book->total; i++)
+    {
+        if (strcmp(book->entries[i].key, key) == 0)
+        {
+            return &(book->entries[i]);
+        }
+    }
+    return NULL;
+}
+void printAll(SymbolTable *book)
+{
+    for (int i = 0; i < book->total; i++)
+    {
+        printf("%s %ld\n", (char *)(book->entries[i]).key, *(long *)book->entries[i].value);
+    }
+}
+int main()
+{
+    SymbolTable *table = createSymbolTable(makePhone, comparePhone);
+    int choice;
+    char *name;
+    long num;
+    while (1)
+    {
+        printf("______SYMBOL_TABLE______\n");
+        printf("Choose your choice:\n");
+        printf("1. Add entry to table \n");
+        printf("2. Drop table \n");
+        printf("3. Get detail entry \n");
+        printf("4. Out\n");
+        printf("5. Print table\n");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("Input name: \n");
+            scanf("%s", name);
+            printf("Input phone number: \n");
+            scanf("%ld", &num);
+            addEntry(name, &num, table);
+            break;
+        case 2:
+            dropSymbolTable(table);
+            return 0;
+        case 3:
+            printf("Input name: \n");
+            scanf("%s", name);
+            Entry *needEntry = getEntry(name, table);
+            printf("Found object : %s %d\n", (char *)needEntry->key, *(long *)needEntry->value);
+            break;
+        case 4:
+            return 0;
+        case 5:
+            printAll(table);
+            break;
+        }
+    }
+}
