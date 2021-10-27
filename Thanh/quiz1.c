757cac1fdf1b72e46a061f9f3253abd66cfa5a27
Trần Phúc Thành <enigma15012001@gmail.com>
2021-10-27 13:24:30 +0700

    check

--- Thanh/quiz1.c
+++ Thanh/quiz1.c
@@ -1,139 +1,137 @@
-#include<stdio.h>
-#include<stdbool.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
 #include "jrb.h"
-//------------------------------------------------------------
-JRB jrb_find_key(JRB tree, Jval value)
+#include "jval.h"
+#include <string.h>
+#include <stdlib.h>
+JRB find_number(JRB head_node, long number)
 {
-  JRB temp;
-  jrb_traverse(temp, tree){
-    if (jval_l(temp->val) == jval_l(value))
-      return temp;
+  JRB tmp;
+  jrb_traverse(tmp, head_node)
+  {
+    if (jval_l(tmp->val) == number)
+    {
+      return tmp;
+    }
   }
-
   return NULL;
 }
-
-//------------------------------------------------------------
-void delete_node(JRB tree, char* key)
+JRB find_name(JRB head_node, char *name)
 {
-  JRB temp = jrb_find_str(tree, key);
-
-  if (temp != NULL){
-    jrb_delete_node(temp);
+  JRB tmp;
+  jrb_traverse(tmp, head_node)
+  {
+    if (strcmp(jval_s(tmp->key), name) == 0)
+    {
+      return tmp;
+    }
   }
-  else{
-    printf("Key invalid! \n");
-  } 
+  return NULL;
 }
-
-//-------------------------------------------------------------------
-void jrb_print_db(JRB book)
+void modifyNumber(JRB new_node, long number)
 {
-    JRB temp;
-  
-    jrb_traverse(temp, book){
-        printf("%s\t%ld\n", jval_s(temp->key), jval_l(temp->val));
-    }
+  JRB node = new_node;
+  node->val = new_jval_l(number);
+}
+void add_phone_number(JRB *head_node, char *name, long number)
+{
+  if (find_name(*head_node, name))
+  {
+    modifyNumber(find_name(*head_node, name), number);
+  }
+  else
+  {
+    jrb_insert_str(*head_node, name, new_jval_l(number));
+  }
+}
+void jrb_print_db(JRB head_node)
+{
+  JRB tmp;
+  jrb_rtraverse(tmp, head_node)
+  {
+    printf("Name: %s - Phone number: %ld\n", jval_s(tmp->key), jval_l(tmp->val));
+  }
+}
+void delete (JRB head_node)
+{
+  JRB temp;
+  jrb_traverse(temp, head_node)
+  {
+    free(jval_s(temp->key));
+  }
+  jrb_free_tree(head_node);
 }
 
-//------------------------------------------------------------
-void modify_phone(JRB book, char *name){
-    JRB temp = jrb_find_str(book, name);
-    if (temp == NULL) printf("%s is not existed!\n", name);
-    else {
-        long phone;
-        printf("Input new phonenumber: ");
-        scanf("%ld", &phone);
-        temp->val = new_jval_l(phone);
-    }
+void deleteName(JRB *new_node)
+{
+  jrb_delete_node(*new_node);
 }
-//------------------------------------------------------------
+void load_file(JRB head_node)
+{
+  FILE *fp;
+  char name[100];
+  long number;
+  if ((fp = fopen("test.txt", "r")) == NULL)
+  {
+    printf("Error! opening file");
+    exit(1);
+  }
+  while (!feof(fp))
+  {
+    fscanf(fp, "%s %ld", name, &number);
+    add_phone_number(&head_node, strdup(name), number);
+  }
+  jrb_print_db(head_node);
+  fclose(fp);
+}
 int main()
 {
-  JRB phonebook, node;
+  JRB head_node = make_jrb();
   int choice;
-  char new_name[100], name_to_find[100], temp[100];
-  long new_num;
+  char name[100];
+  long number;
 
-  phonebook = make_jrb();
-
-  while (1) {
-    printf("------MENU------\n");
-    printf("1. Add entry to table \n");
-    printf("2. Print table \n");
-    printf("3. Modify phone number \n");
-    printf("4. Find phone by name\n");
-    printf("5. Fine name by phone number\n");
-    printf("6. Delete\n");
-    printf("7. Exit\n");
-    printf("----------------\n");
-    printf("Enter your choice: ");
+  while (1)
+  {
+    printf("1. Add phone number\n");
+    printf("2. Modify number\n");
+    printf("3. Delete a name based on number\n");
+    printf("4. Print data\n");
+    printf("5. Read data from file\n");
+    printf("6. Feel bored? Get out\n");
+    printf("Input your choice: \n");
     scanf("%d", &choice);
     switch (choice)
     {
     case 1:
-      printf("Input name: ");
-      printf("%s\n", new_name);
-
-      scanf("%s", new_name);
-
-      printf("%s\n", new_name);
-      strcpy(temp, new_name);
-      printf("Input phone number: ");
-      scanf("%ld", &new_num);
-      jrb_insert_str(phonebook, temp, new_jval_l(new_num));
-      printf("%s\n", new_name);
+      printf("Input name and number: \n");
+      scanf("%s %ld", name, &number);
+      add_phone_number(&head_node, strdup(name), number);
+      break;
+    case 4:
+      jrb_print_db(head_node);
       break;
     case 2:
-      jrb_print_db(phonebook);
+      printf("Input name: \n");
+      scanf("%s", name);
+      JRB change_number = find_name(head_node, name);
+      printf("Input new number for changing: \n");
+      scanf("%ld", &number);
+      modifyNumber(change_number, number);
+      jrb_print_db(head_node);
       break;
     case 3:
-      printf("Enter name to modify number:");
-      scanf("%s", name_to_find);
-      modify_phone(phonebook, name_to_find);
+      printf("Input number: \n");
+      scanf("%ld", &number);
+      JRB change_numbers = find_number(head_node, number);
+      deleteName(&change_numbers);
+      jrb_print_db(head_node);
       break;
-    default:
-      return 1;
+    case 5:
+      load_file(head_node);
       break;
+    case 6:
+      delete (head_node);
+      return 0;
     }
-  }
-
-
-  jrb_insert_str(phonebook, "Thanh", new_jval_l(965));
-  jrb_insert_str(phonebook, "Duc", new_jval_l(966));
-  jrb_insert_str(phonebook, "Khue", new_jval_l(9617));
-  jrb_insert_str(phonebook, "Dang", new_jval_l(967));
-  jrb_insert_str(phonebook, "Trinh", new_jval_l(96127));
-
-  jrb_print_db(phonebook);
-  printf("\n");
-
-  printf("Find phone by name: \n");
-  printf("  - (Thanh) %d\n", jrb_find_str(phonebook, "Thanh")->val);
-  printf("  - (Duc) %d\n", jrb_find_str(phonebook, "Duc")->val);
-  printf("\n");
-
-  printf("Find name by phone number: \n");
-  printf("(966) %s\n", jrb_find_key(phonebook, (Jval) 966)->key);
-  printf("(96127) %s\n", jrb_find_key(phonebook, (Jval) 96127)->key);
-  printf("\n");
-  
-  printf("Modify: \n");
-  modify_phone(phonebook, "Duc");
-  printf("\n");
-
-  printf("Delete a node: \n");
-  delete_node(phonebook, "Thanh");
-  jrb_print_db(phonebook);
-  printf("\n");
-
-
-  printf("Free phonebook tree\n");
-  jrb_free_tree(phonebook);
-  printf("--> Deleted!\n");
-  printf("\n");
-  
-  return 0;
+  };
 }
