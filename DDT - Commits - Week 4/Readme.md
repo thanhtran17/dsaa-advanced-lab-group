058f8e89d18f13ef4a4eeac4d0f857668881c2c1
Trần Phúc Thành <enigma15012001@gmail.com>
2021-10-27 21:45:52 +0700

    done week 4

---
+++ DDT - Commits - Week 4/Readme.md
@@ -0,0 +1,4 @@
+How to run the files:
+
+- Open terminal on the right folder
+- Type this to run: & "./run.ps1"
---
+++ DDT - Commits - Week 4/run.ps1
@@ -0,0 +1,5 @@
+gcc -c jval.c
+gcc -c jrb.c 
+gcc -c DDT_W4_Q1.c
+gcc -o week4 jval.o jrb.o DDT_W4_Q1.o
+./week4
