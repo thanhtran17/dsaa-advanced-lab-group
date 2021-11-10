f14b3481aa457a4d09135ddd7fce2d44b9324c33
Trần Phúc Thành <enigma15012001@gmail.com>
2021-11-10 23:25:17 +0700

    done week 6

---
+++ Commits Week 6/run.ps1
@@ -0,0 +1,5 @@
+gcc -c DDT_W6_Q2.c
+gcc -c jval.c
+gcc -c jrb.c
+gcc -o quiz2 DDT_W6_Q2.o jval.o jrb.o
+./quiz2
--- Thanh/main.c
+++ Thanh/main.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 //---------------------------------------------------
 typedef struct {
-  int* matrix;
+  int *matrix;
   int size;
 } Graph;
 //---------------------------------------------------
@@ -51,82 +51,34 @@ void deleteGraph(Graph graph)
 //---------------------------------------------------
 int main()
 {
-  int i, n, output[10], choice;
-  int xi, yi, enteredAdjacent; 
+  int i, n, output[10];
+  Graph g= createGraph(10);
+  addEdge(g, 0, 1);
+  addEdge(g, 0, 2);
+  addEdge(g, 1, 2);
+  addEdge(g, 1, 3); 
 
-  Graph g = createGraph(10);
-
-  printf("---Menu---\n");
-  printf("1. Add edge\n");
-  printf("2. Print graph\n");
-  printf("3. Find adjacent vertices\n");
-  printf("4. Delete graph and exit\n");
-
-  while (1) {
-    printf("Enter your choice: ");
-    scanf("%d", &choice);
-
-    switch (choice){  
-      case 1: 
-        printf("--> Enter edge (x, y): ");
-        scanf("%d%d", &xi, &yi);
-        addEdge(g, xi, yi);
-        printf("\n");
-        break;
-      case 2: 
-        printf("--> Graph:\n\n");
-        for (int i = 0; i < 10; i++){
-          for (int j = 0; j < 10; j++)  
-            printf("%5d", g.matrix[i * 10 + j]);
-          printf("\n");
-        }
-        printf("\n");
-        break;
-      case 3:
-        printf("--> Enter vertice to find adjacent vertices: ");
-        scanf("%d", &enteredAdjacent);
-        n = getAdjacentVertices (g, enteredAdjacent, output);
-
-        if (n == 0) {
-          printf("No adjacent vertices of the selected node\n");
-        } 
-        else {
-          printf("Adjacent vertices of the selected node:\n");
-          for (i=0; i<n; i++)   
-            printf("%2d", output[i]);
-          printf("\n");
-        }
-        break;
-      case 4: 
-        deleteGraph(g);
-        printf("--> Graph deleted");
-        return 0;
-    }
+  //Print graph  
+  printf("Graph:\n\n");
+  for (int i = 0; i < 10; i++){
+    for (int j = 0; j < 10; j++)  
+      printf("%5d", g.matrix[i * 10 + j]);
+    printf("\n");
   }
+
+  // Adjacent vertices  
+  n = getAdjacentVertices (g, 3, output);
+
+  if (n == 0) {
+    printf("No adjacent vertices of the selected node\n");
+  } 
+  else {
+    printf("\nAdjacent vertices of the selected node:");
+    for (i=0; i<n; i++)   
+      printf("%2d", output[i]);
+  }
+
+  printf("\n\n");
+
+  return 0;
 }
-  // addEdge(g, 0, 1);
-  // addEdge(g, 0, 2);
-  // addEdge(g, 1, 2);
-  // addEdge(g, 1, 3);
-
-  // //Print graph  
-  // printf("Graph:\n\n");
-  // for (int i = 0; i < 10; i++){
-  //   for (int j = 0; j < 10; j++)  
-  //     printf("%5d", g.matrix[i * 10 + j]);
-  //   printf("\n");
-  // }
-
-  // // Adjacent vertices  
-  // n = getAdjacentVertices (g, 3, output);
-
-  // if (n == 0) {
-  //   printf("No adjacent vertices of the selected node\n");
-  // } 
-  // else {
-  //   printf("\nAdjacent vertices of the selected node:");
-  //   for (i=0; i<n; i++)   
-  //     printf("%2d", output[i]);
-  // }
-
-  // printf("\n\n");
