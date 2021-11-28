747ed848e7eaee43eb707d0e0b9ab91a9be85881
Your Name <longdieu12x@gmail.com>
2021-11-28 16:33:01 +0700

    Almost finished still soundex error

--- Dictionary-Group/dictionary.sh
+++ Dictionary-Group/dictionary.sh
@@ -1,2 +1,6 @@
-gcc -o dictionary dictionary.c `pkg-config --cflags gtk+-3.0` `pkg-config --libs gtk+-3.0` 
-./dictionary
+rm -f test
+gcc -Wall -c soundex.c
+gcc -c dictionary.c `pkg-config --cflags gtk+-3.0` `pkg-config --libs gtk+-3.0` -Lbtree/lib -lbt -Ibtree/inc 
+gcc dictionary.o soundex.o -o test `pkg-config --cflags gtk+-3.0` `pkg-config --libs gtk+-3.0` -Lbtree/lib -lbt -Ibtree/inc 
+./test
+
---
+++ Dictionary-Group/soundex.h
@@ -0,0 +1,2 @@
+extern const char *soundex(const char *s);
+
