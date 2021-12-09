0c4e48e32648898f7bae2647d55be146dda9e35c
Your Name <longdieu12x@gmail.com>
2021-12-09 14:17:04 +0700

    Add week-10 assignment

--- Dang/quiz.c
+++ Dang/quiz.c
@@ -5,22 +5,42 @@
 #include "jval.h"
 #include "stack.h"
 #include "dllist.h"
+#include <math.h>
+#include <stdbool.h>
+struct item
+{
+  double val;
+  int parent;
+};
+int edge_number = 0;
+
 typedef struct
 {
   JRB edges;
   JRB vertices;
 } Graph;
+void swap(int a, int b)
+{
+  int temp = a;
+  a = b;
+  b = temp;
+}
 Graph createGraph();
 void addVertex(Graph graph, int id, char *name);
 char *getVertex(Graph graph, int id);
-void addEdge(Graph graph, int v1, int v2);
-int hasEdge(Graph graph, int v1, int v2);
+void addEdge(Graph graph, int v1, int v2, double weight);
+double getEdgeValue(Graph graph, int v1, int v2);
 int indegree(Graph graph, int v, int *output);
 int outdegree(Graph graph, int v, int *output);
 int DAG(Graph graph);
 void printVertex(Graph graph, int id);
 int DFS(Graph graph, int start);
 void topologicalSort(Graph g, int *output, int *n);
+double shortestPath(Graph graph, int s, int t, int *path, int *length);
+int minDistance(Graph graph, double dist[], bool sptSet[]);
+void printSolution(Graph graph, double dist[]);
+void dijkstra(Graph graph, int src);
+
 int main()
 {
   Graph graph = createGraph();
@@ -31,88 +51,166 @@ int main()
   int v1, v2;
   int id;
   char name[256];
-  do
+  double weight;
+  int path[256];
+  int length;
+  addVertex(graph, 1, "s");
+  addVertex(graph, 2, "2");
+  addVertex(graph, 3, "3");
+  addVertex(graph, 4, "4");
+  addVertex(graph, 5, "5");
+  addVertex(graph, 6, "6");
+  addVertex(graph, 7, "7");
+  addVertex(graph, 8, "t");
+  addEdge(graph, 1, 2, 9);
+  addEdge(graph, 2, 3, 24);
+  addEdge(graph, 1, 6, 14);
+  addEdge(graph, 6, 3, 18);
+  addEdge(graph, 6, 5, 30);
+  addEdge(graph, 5, 4, 11);
+  addEdge(graph, 4, 8, 6);
+  addEdge(graph, 4, 3, 6);
+  addEdge(graph, 3, 5, 2);
+  addEdge(graph, 3, 8, 19);
+  addEdge(graph, 5, 8, 16);
+  addEdge(graph, 1, 7, 15);
+  addEdge(graph, 7, 6, 5);
+  addEdge(graph, 7, 5, 20);
+  addEdge(graph, 7, 8, 44);
+  printf("Value: %g\n", shortestPath(graph, 1, 8, path, &length));
+  for (int i = length; i >= 0; i--)
   {
-    printf("1. Add Vertex\n");
-    printf("2. Add Edges\n");
-    printf("3. Has Edge\n");
-    printf("4. Get Vertex\n");
-    printf("5. Indegree\n");
-    printf("6. Outdegree\n");
-    printf("7. Topological sort\n");
-    printf("8. Out\n");
-    printf("Input choice: \n");
-    scanf("%d", &choice);
-    switch (choice)
+    printf("%s->", getVertex(graph, path[i]));
+  }
+  printf("t\n");
+  // do
+  // {
+  //   printf("1. Add Vertex\n");
+  //   printf("2. Add Edges\n");
+  //   printf("3. Has Edge\n");
+  //   printf("4. Get Vertex\n");
+  //   printf("5. Indegree\n");
+  //   printf("6. Outdegree\n");
+  //   printf("7. Topological sort\n");
+  //   printf("8. Out\n");
+  //   printf("Input choice: \n");
+  //   scanf("%d", &choice);
+  //   switch (choice)
+  //   {
+  //   case 1:
+  //     printf("Input id and name: \n");
+  //     scanf("%d %s", &id, name);
+  //     addVertex(graph, id, name);
+  //     break;
+  //   case 2:
+  //     printf("Input 2 vertices v1 and v2: \n");
+  //     scanf("%d %d %g", &v1, &v2, &weight);
+  //     addEdge(graph, v1, v2, weight);
+  //     break;
+  //   case 3:
+  //     printf("Input 2 vertices v1 and v2: \n");
+  //     scanf("%d %d", &v1, &v2);
+  //     total = getEdgeValue(graph, v1, v2);
+  //     if (total == 1)
+  //     {
+  //       printf("There exists edge between %d and %d\n", v1, v2);
+  //     }
+  //     else
+  //     {
+  //       printf("There doesn't exist edge between %d and %d\n", v1, v2);
+  //     }
+  //     break;
+  //   case 4:
+  //     printf("Input id: \n");
+  //     scanf("%d", &id);
+  //     if (getVertex(graph, id) == NULL)
+  //     {
+  //       printf("There doesn't exist vertex %d", id);
+  //     }
+  //     else
+  //     {
+  //       printf("Name of vertex is %s\n", getVertex(graph, id));
+  //     }
+  //     break;
+  //   case 5:
+  //     printf("Input id: \n");
+  //     scanf("%d", &id);
+  //     printf("There are %d vertices to %d\n", indegree(graph, id, output), id);
+  //     break;
+  //   case 6:
+  //     printf("Input id: \n");
+  //     scanf("%d", &id);
+  //     if (outdegree(graph, id, output) != -1)
+  //     {
+  //       printf("There are %d vertices from %d\n", outdegree(graph, id, output), id);
+  //     }
+  //     else
+  //     {
+  //       printf("No vertices go from %d vertex \n", id);
+  //     }
+  //     break;
+  //   case 8:
+  //     return 0;
+  //   case 7:
+  //     if (DAG(graph) == 1)
+  //     {
+  //       printf("Can't make topological sort because of circle graph\n");
+  //       break;
+  //     }
+  //     topologicalSort(graph, output, &n);
+  //     for (int i = 0; i < n; i++)
+  //     {
+  //       printf("%s\n", getVertex(graph, output[i]));
+  //     }
+  //     break;
+  //   }
+  // } while (1);
+}
+int minDistance(Graph graph, double dist[], bool sptSet[])
+{
+  JRB node = NULL;
+  double min = INFINITY, min_index;
+  jrb_traverse(node, graph.vertices)
+  {
+    int v = jval_i(node->key);
+    if (sptSet[v] == false && dist[v] <= min)
+      min = dist[v],
+      min_index = v;
+  }
+  return min_index;
+}
+void printSolution(Graph graph, double dist[])
+{
+  JRB node = NULL;
+  printf("Vertex \t\t Distance from Source\n");
+  jrb_traverse(node, graph.vertices)
+  {
+    int i = jval_i(node->key);
+    printf("%s \t\t %g\n", getVertex(graph, i), dist[i]);
+  }
+}
+
+void dijkstra(Graph graph, int src)
+{
+  JRB node = NULL;
+  double dist[100];
+  bool sptSet[100];
+  for (int i = 0; i < 100; i++)
+    dist[i] = INFINITY, sptSet[i] = false;
+  dist[src] = 0;
+  jrb_traverse(node, graph.vertices)
+  {
+    JRB tree = NULL;
+    int u = minDistance(graph, dist, sptSet);
+    sptSet[u] = true;
+    jrb_traverse(tree, graph.vertices)
     {
-    case 1:
-      printf("Input id and name: \n");
-      scanf("%d %s", &id, name);
-      addVertex(graph, id, name);
-      break;
-    case 2:
-      printf("Input 2 vertices v1 and v2: \n");
-      scanf("%d %d", &v1, &v2);
-      addEdge(graph, v1, v2);
-      break;
-    case 3:
-      printf("Input 2 vertices v1 and v2: \n");
-      scanf("%d %d", &v1, &v2);
-      total = hasEdge(graph, v1, v2);
-      if (total == 1)
-      {
-        printf("There exists edge between %d and %d\n", v1, v2);
-      }
-      else
-      {
-        printf("There doesn't exist edge between %d and %d\n", v1, v2);
-      }
-      break;
-    case 4:
-      printf("Input id: \n");
-      scanf("%d", &id);
-      if (getVertex(graph, id) == NULL)
-      {
-        printf("There doesn't exist vertex %d", id);
-      }
-      else
-      {
-        printf("Name of vertex is %s\n", getVertex(graph, id));
-      }
-      break;
-    case 5:
-      printf("Input id: \n");
-      scanf("%d", &id);
-      printf("There are %d vertices to %d\n", indegree(graph, id, output), id);
-      break;
-    case 6:
-      printf("Input id: \n");
-      scanf("%d", &id);
-      if (outdegree(graph, id, output) != -1)
-      {
-        printf("There are %d vertices from %d\n", outdegree(graph, id, output), id);
-      }
-      else
-      {
-        printf("No vertices go from %d vertex \n", id);
-      }
-      break;
-    case 8:
-      return 0;
-    case 7:
-      if (DAG(graph) == 1)
-      {
-        printf("Can't make topological sort because of circle graph\n");
-        break;
-      }
-      topologicalSort(graph, output, &n);
-      for (int i = 0; i < n; i++)
-      {
-        printf("%s\n", getVertex(graph, output[i]));
-      }
-      break;
+      int v = jval_i(tree->key);
+      if (!sptSet[v] && getEdgeValue(graph, u, v) && dist[u] != INFINITY && dist[u] + getEdgeValue(graph, u, v) < dist[v])
+        dist[v] = dist[u] + getEdgeValue(graph, u, v);
     }
-  } while (1);
+  }
+  printSolution(graph, dist);
 }
 Graph createGraph()
 {
@@ -123,6 +221,7 @@ Graph createGraph()
 }
 void addVertex(Graph graph, int id, char *name)
 {
+  edge_number++;
   JRB node = jrb_find_int(graph.vertices, id);
   if (node == NULL)
   {
@@ -136,7 +235,7 @@ char *getVertex(Graph graph, int id)
   JRB node = jrb_find_int(graph.vertices, id);
   return (node == NULL) ? NULL : jval_s(node->val);
 }
-void addEdge(Graph graph, int v1, int v2)
+void addEdge(Graph graph, int v1, int v2, double weight)
 {
   // Add to vertex 1
   JRB node = jrb_find_int(graph.edges, v1);
@@ -144,21 +243,21 @@ void addEdge(Graph graph, int v1, int v2)
   {
     JRB tree = make_jrb();
     jrb_insert_int(graph.edges, v1, new_jval_v(tree));
-    jrb_insert_int(tree, v2, new_jval_i(1));
+    jrb_insert_int(tree, v2, new_jval_d(weight));
   }
   else
   {
     JRB tree = (JRB)jval_v(node->val);
-    jrb_insert_int(tree, v2, new_jval_i(1));
+    jrb_insert_int(tree, v2, new_jval_d(weight));
   }
 }
-int hasEdge(Graph graph, int v1, int v2)
+double getEdgeValue(Graph graph, int v1, int v2)
 {
   JRB node = jrb_find_int(graph.edges, v1);
   if (node == NULL)
     return 0;
   JRB endPoint = jrb_find_int(jval_v(node->val), v2);
-  return (endPoint == NULL) ? 0 : 1;
+  return (endPoint == NULL) ? 0 : jval_d(endPoint->val);
 }
 int indegree(Graph graph, int v, int *output)
 {
@@ -283,4 +382,93 @@ void topologicalSort(Graph graph, int *output, int *n)
       }
     }
   }
+};
+void heapify(int arr[], int n, int i, struct item node[])
+{
+  int smallest = i;
+  int l = 2 * i + 1;
+  int r = 2 * i + 2;
+  if (l < n && arr[l] < arr[smallest])
+    smallest = l;
+  if (r < n && arr[r] < arr[smallest])
+    smallest = r;
+  if (smallest != i)
+  {
+    swap(arr[i], arr[smallest]);
+    heapify(arr, n, smallest, node);
+  }
+}
+
+void heapSort(int arr[], int n, struct item node[])
+{
+  for (int i = n / 2 - 1; i >= 0; i--)
+    heapify(arr, n, i, node);
+
+  for (int i = n - 1; i >= 0; i--)
+  {
+    swap(arr[0], arr[i]);
+
+    heapify(arr, i, 0, node);
+  }
+}
+void insertionSort(int a[], int n, struct item node[])
+{
+  int temp;
+  int j;
+  for (int i = 0; i < n; i++)
+  {
+    temp = a[i];
+    for (j = i - 1; j >= 0; j--)
+    {
+      if (node[a[j]].val < node[temp].val)
+        a[j + 1] = a[j];
+      else
+        break;
+    }
+    a[j + 1] = temp;
+  }
+}
+double shortestPath(Graph graph, int s, int t, int *path, int *length)
+{
+  JRB node;
+  struct item node_arr[100];
+  int queue[1000];
+  int number = 0;
+  *length = 0;
+  int u;
+  int v;
+  int total;
+  int outdegree_arr[30];
+  jrb_traverse(node, graph.vertices)
+  {
+    queue[number++] = jval_i(node->key);
+    node_arr[jval_i(node->key)].val = INFINITY;
+    node_arr[jval_i(node->key)].parent = -1;
+  };
+  node_arr[s].val = 0;
+  while (number != 0)
+  {
+    insertionSort(queue, number, node_arr);
+    u = queue[number - 1];
+    total = outdegree(graph, u, outdegree_arr);
+    for (int k = 0; k < total; k++)
+    {
+      v = outdegree_arr[k];
+      if (node_arr[v].val > node_arr[u].val + getEdgeValue(graph, u, v))
+      {
+        node_arr[v].val = node_arr[u].val + getEdgeValue(graph, u, v);
+        node_arr[v].parent = u;
+      }
+    }
+    number--;
+  };
+  struct item destination = node_arr[t];
+  int total_weight = destination.val;
+  while (destination.parent != -1)
+  {
+    int a = destination.parent;
+    path[(*length)++] = a;
+    destination = node_arr[a];
+  }
+  return total_weight;
 };
--- Thanh/run.ps1
+++
@@ -1,6 +0,0 @@
-gcc -c 1.c
-gcc -c dllist.c
-gcc -c jrb.c
-gcc -c jval.c
-gcc -o 1 1.o dllist.o jrb.o jval.o
-./1
